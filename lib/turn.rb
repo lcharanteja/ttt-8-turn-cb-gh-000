6d0e0cfd29054b9e0b7edf2a63c31fc43ec7ddb8
Charan Teja Lellaboyena <teja.charan20@gmail.com>
2017-11-10 01:41:08 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -9,3 +9,15 @@ end
 def input_to_index(user_input)
   user_input.to_i - 1
 end
+
+def valid_move?(board,index)
+  index.between?(0,8) && position_taken?(board,index)
+end
+
+def position_taken?(board,index)
+  if(board[index] == "" || board[index] == " " || board[index] == nil)
+    true
+  elsif (board[index] == "X" || board[index] == "O")
+    false
+  end    
+end
